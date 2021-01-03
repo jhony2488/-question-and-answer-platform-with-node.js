0713dabf381addad7e302cd8071628beb2ae8cb0
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-01-02 21:43:53 -0300

    config bootstrap

--- index.js
+++ index.js
@@ -3,21 +3,11 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.get('/:nome/:lang', (req, res) => {
-  let nome = req.params.nome
-  let lang = req.params.lang
-  let exibirMsg = false
-  let produtos = [
-    { nome: 'coca', preco: 5.5 },
-    { nome: 'biscoito', preco: 1.5 },
-    { nome: 'leite', preco: 5 },
-  ]
-  res.render('index', {
-    nome,
-    lang,
-    exibirMsg,
-    produtos,
-  })
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/perguntar', (req, res) => {
+  res.render('perguntar')
 })
 
 app.listen(8080, () => {
