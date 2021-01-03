59e6a64d636b0ad507bed05b03d3741c88e184e2
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-01-03 11:35:33 -0300

    .gitignore

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 app.get('/perguntar', (req, res) => {
   res.render('perguntar')
+})
+app.post('/salvarpergunta', (req, res) => {
+  let title = req.body.title
+  let description = req.body.description
+  res.send(
+    `formulario recebido  <br/> title: ${title} description: ${description}`
+  )
 })
 
 app.listen(8080, () => {
